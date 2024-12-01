6d0b47a74f3332947e7e5110bffc6e901dc83699
Swimnato <66497456+Swimnato@users.noreply.github.com>
2024-12-01 14:17:28 -0700

    added selection rectangle

--- game-jam-2024/scripts/selection.gd
+++ game-jam-2024/scripts/selection.gd
@@ -1,14 +1,38 @@
-extends Polygon2D
+extends Node2D
 
+var nativeResolution = Vector2(1152, 648)
+var startPos = Vector2(0,0);
+var endPos = Vector2(0,0);
+var isMouseDown = false
 
-var startPos = Vector2(0,0)
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	queue_redraw()
+
+func _draw() -> void:
+	if isMouseDown:
+		endPos = convertToRelativeCoordinates(get_global_mouse_position());
+		print(endPos)
+	if endPos != startPos:
+		var size = (startPos - endPos)
+		draw_rect(Rect2(endPos, size), Color.SKY_BLUE, false, 10)
+	
+	
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			startPos = convertToRelativeCoordinates(get_global_mouse_position());
+			isMouseDown = true
+		elif event.is_released():
+			isMouseDown = false
+			startPos = Vector2(0,0)
+			endPos = Vector2(0,0)
+
+func convertToRelativeCoordinates(coords) -> Vector2:
+	var newX = (coords.x - (nativeResolution.x))
+	var newY = (coords.y - (nativeResolution.y))
+	return Vector2(newX,newY)
