26be73cf645ad02a1a0ce5025ad9f6d6400955e5
Swimnato <66497456+Swimnato@users.noreply.github.com>
2024-12-01 16:26:32 -0700

    added selection code

--- game-jam-2024/scripts/Asteroid_Main.gd
+++ game-jam-2024/scripts/Asteroid_Main.gd
@@ -4,17 +4,13 @@ extends Sprite2D
 const AsteroidMovement = preload("res://scripts/AsteroidMovement.gd");
 var movement = AsteroidMovement.new();
 
-var lastUpdate = 0
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	lastUpdate = Time.get_ticks_msec()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	lastUpdate = Time.get_ticks_msec()
-
 	set_position(movement.updatePosition(position, delta * 1000));
 
 	if position.x == position.y and position.y == 0:
