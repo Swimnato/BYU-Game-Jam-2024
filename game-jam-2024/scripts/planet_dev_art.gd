e279ae13a25e00034bc76d056d462fc3f3176fae
Swimnato <66497456+Swimnato@users.noreply.github.com>
2024-12-04 22:29:25 -0700

    added planet animation

--- game-jam-2024/scripts/planet_dev_art.gd
+++ game-jam-2024/scripts/planet_dev_art.gd
@@ -7,6 +7,16 @@ extends Sprite2D
 @onready var healthBar = $Status_Bars;
 @onready var asteroids = $"../asteroidController".asteroids;
 
+var planet_broken = preload("res://art/planet_megabroken.png");
+var planet_hurt = preload("res://art/planet_hurt.png");
+var planet_flashing = preload("res://art/planet_healthy_flash.png");
+var planet_idle = [preload("res://art/planet_healthy_1.png"), preload("res://art/planet_healthy_2.png"), preload("res://art/planet_healthy_3.png"), preload("res://art/planet_healthy_2.png")];
+var currentFrame = 0;
+@export var timeForEachFrame = .5;
+var timeSinceLastFrameChange = 0;
+var lastFlash = 0;
+var rotationsForFlash = 7;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position.x = 0
@@ -20,6 +30,24 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	healthBar.setCurrentHealth(current_hp);
+	if(current_hp > max_hp/3*2):
+		timeSinceLastFrameChange += delta;
+		if(timeSinceLastFrameChange > timeForEachFrame):
+			timeSinceLastFrameChange = 0;
+			currentFrame += 1;
+			if(currentFrame >= planet_idle.size()):
+				currentFrame = 0;
+			if(currentFrame % 2 == 1):
+				lastFlash += 1;
+		if(lastFlash >= rotationsForFlash):
+			texture = planet_flashing;
+			lastFlash = 0;
+		else:
+			texture = planet_idle[currentFrame];
+	elif(current_hp > max_hp/3):
+		texture = planet_hurt;
+	else:
+		texture = planet_broken;
 	#pass
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
